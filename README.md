618b175f10843f55212af1f5acd7981443e2d81b
tianqi-li <tianqi.li@dena.com>
2016-12-07 16:27:29 +0900

    phantomjsによるフルサイズ画像ダウンロードを追加

--- README.md
+++ README.md
@@ -7,18 +7,33 @@ pip install bs4
 pip install urllib
 ```
 
-## 画像ダウンロード
-以下のコマンドで、googleからcatというキーワードで画像検索した結果をローカルにダウンロードする。
+## サブネイル画像ダウンロード
+以下のコマンドで、googleからcatというキーワードで画像検索した結果をローカルにダウンロードする。(サイズの小さいサブネイル画像のみ)
 
 ```
-python download_image.py "cat"
+python download_thumbnail.py "cat"
 ```
 
 以下のオプションで、画像検索結果を1000件、ローカルの./cat_dir/というディレクトリにダウンロードする(ディレクトリ指定しない場合、ダウンロード先は./images/になる)。
 
 ```
-python download_image.py "cat" -n 1000 -d "cat_dir"
+python download_thumbnail.py "cat" -n 1000 -d "cat_dir"
 ```
+
+## フルサイズ画像ダウンロード
+サブネイル画像のダウンロードと同様に、以下のコマンドで、フルサイズの画像をダウンロードできる。ただし、指定枚数は最大100枚までしかダウンロードできない。
+
+```
+python download_image.py "cat" -n 50 -d "cat_dir"
+```
+
+スクレイピングの際phantomjsを使うので、インストールしていない場合は、brewでインストールしておく。
+
+```
+brew install phantomjs
+```
+
+
 
 ## imagenet 1000カテゴリダウンロード
 以下のコマンドで、ImageNetの1000カテゴリをgoogleで画像検索し、それぞれ上位5枚を保存する。`download_1000_categories.py`を直接編集すれば、好きな枚数ダウンロード可能。
--- download_image.py
+++ download_image.py
@@ -3,30 +3,24 @@ import argparse
 import urllib.request
 import bs4
 
-# google画像検索関数
-def google_search_image(keyword, start_from):
-    image_urls = []
-    url = 'https://www.google.co.jp/search?q=%s&source=lnms&tbm=isch&tbs=isz:l&start=%d' % (urllib.request.quote(args.keyword), start_from)
-    req = urllib.request.Request(
-        url,
-        data = None,
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)'
-        }
-    )
-    soup = bs4.BeautifulSoup(urllib.request.urlopen(req).read(), "html.parser")
-
-    for img in soup.find_all('img'):
-        src = img.get('src')
-        if src:
-            image_urls.append(src)
-
-    return image_urls
+# コマンドライン実行メソッド
+def exec_cmd(cmd):
+    from subprocess import Popen, PIPE
+    p = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    lines = str(out).split('\\n')[:-1]
+    lines[0] = lines[0].split('\'')[1]
+    return lines
 
 # 画像URLダウンロード関数
 def download_image(url, file_path):
     print('downloading image %s into %s' % (url, file_path))
-    urllib.request.urlretrieve(url, file_path)
+    try:
+        urllib.request.urlretrieve(url, file_path)
+        return True
+    except:
+        print('※link not found')
+        return False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='download images from google')
@@ -44,13 +38,10 @@ if __name__ == '__main__':
     print('searching keyword [%s] ...' % (args.keyword))
     image_urls = []
     n_images = int(args.num)
-    while len(image_urls) < n_images:
-        search_result = google_search_image(args.keyword, len(image_urls))
-        search_result.reverse()
-        while len(image_urls) < n_images and len(search_result) > 0:
-            image_urls.append(search_result.pop())
-
-    # 画像ダウンロード
-    for image_url in image_urls:
-        file_path = './%s/%s.jpg' % (args.dirname, image_url.split(':')[-1])
-        download_image(image_url, file_path)
+    search_result = exec_cmd('phantomjs google_search_image.js "%s"' % args.keyword)
+    search_result.reverse()
+    while len(image_urls) < n_images and len(search_result) > 0:
+        image_url = search_result.pop()
+        file_path = './%s/%s' % (args.dirname, image_url.split('/')[-1])
+        if(download_image(image_url, file_path)):
+            image_urls.append(image_url)
